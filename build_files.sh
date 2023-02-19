9419288cc20506ec95f49eedae544b71a881a990
MD Aamir <88143370+DevelopAamir@users.noreply.github.com>
2023-02-19 10:00:11 +0545

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,6 @@
 echo "BUILD START"
 python --version
-pip install asgiref
-pip install Django
-pip install django-ckeditor
-pip install django-crispy-forms
-pip install django-js-asset
-pip install gunicorn
-pip install Pillow
-pip install sqlparse
+pip install -r requirements.txt
 python manage.py collectstatic
 
 echo "Build Complete"

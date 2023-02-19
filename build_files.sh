7979079ce28861b55d7ce3e4511282cba5aac488
MD Aamir <88143370+DevelopAamir@users.noreply.github.com>
2023-02-19 10:28:48 +0545

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,6 @@
 echo "BUILD START"
 python --version
 pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic
+
 
 echo "Build Complete"
